21e11ff8f238d523ecb13cf8bcc64d67703207dc
andreas-mich <andreas@michalakopoulos.eu>
2022-05-05 20:02:19 +0200

    clean-up

--- cansat-arduino/cansat-rx/src/main.cpp
+++ cansat-arduino/cansat-rx/src/main.cpp
@@ -1,13 +1,3 @@
-// rf69 demo tx rx.pde
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple messageing client
-// with the RH_RF69 class. RH_RF69 class does not provide for addressing or
-// reliability, so you should only use RH_RF69  if you do not need the higher
-// level messaging abilities.
-// It is designed to work with the other example rf69_server.
-// Demonstrates the use of AES encryption, setting the frequency and modem 
-// configuration
-
 #include <SPI.h>
 #include <RH_RF69.h>
 
